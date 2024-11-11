3eb3cc4ceff94decccadd82b4cc632d04d87783b
Erik <erikteixeiraluz@gmail.com>
2024-11-11 20:21:07 -0300

    Configuração de tsx para rodar o servidor diretamente

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://192.168.0.20:3333"
-})
+  baseURL: process.env.BASE_URL || "http://localhost:3333"
+});
